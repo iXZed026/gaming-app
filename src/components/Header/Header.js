053e134a5ff6564ee70d7d8493ba4fcf0b2ae4ec
danyal <danyal.titanka24@gmail.com>
2024-07-04 14:07:45 +0330

    compelete header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,24 +4,24 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-        <div className="header-container">
-            <div className="header-flex">
-                <div className="header-logo">
-                    <img src="../../images/logo.png" alt="" />
-                </div>
-                <nav>
-                  <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">TOURNAMENT</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">CONTACT</a></li>
-                  </ul>
-                  <div className="join-team-button">
-                    <button type="submit">join our team</button>
-                  </div>
-                </nav>
-            </div>
+      <div className="header-container">
+        <div className="header-flex">
+          <div className="header-logo">
+            <img src="../../images/logo.png" alt="" />
+          </div>
+          <nav>
+            <ul>
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">TOURNAMENT</a></li>
+              <li><a href="#">NEWS</a></li>
+              <li><a href="#">CONTACT</a></li>
+            </ul>
+          </nav>
+          <div className="join-team-button">
+            <button type="submit">join our team</button>
+          </div>
         </div>
+      </div>
     </header>
   )
 }
