2d31a15cc11ab7f8df9aae615c72b06c2a5dcd35
danyal <danyal.titanka24@gmail.com>
2024-07-08 16:42:09 +0330

    create contact us component

---
+++ src/components/Contact/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ContactUs.css";
+
+const ContactUs = () => {
+    return (
+        <div className="contact-us">
+            
+        </div>
+    )
+}
+
+export default ContactUs
